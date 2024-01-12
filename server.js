0c268a8a594f0cb835899bcad363c05d840d98f6
AyomiCoder <alukoayomide623@gmail.com>
2024-01-12 19:05:07 +0100

    connect database

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express()
 
 const port = process.env.PORT || 8080
 
-
+// middlewares
 app.use(express.json());
 app.use('/api/contacts', require("./routes/contactRoutes"));
 app.use(errorHandler)
