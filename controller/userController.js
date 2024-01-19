ae68df17613fee022e9fcb0d2c0912b0be5720b9
AyomiCoder <alukostephen01@gmail.com>
2024-01-19 22:03:37 +0100

    update user Controller

--- controller/userController.js
+++ controller/userController.js
@@ -1,24 +1,25 @@
 const asyncHandler = require('express-async-handler');
-const User = require('../model/userModel')
+const User = require('../models/userModel')
 
 
 // @desc Register a user
 // @route POST api/users/register
 // @access public
 const registerUser = asyncHandler( async (req, res) =>{
-const (username, email, password) = req.body
+const {username, email, password} = req.body
 if (!username || !email || !password){
 res.status(400);
-throw New Error("All fields are mandatory")
+throw new Error("All fields are mandatory")
 }
 
 const userAvailable = await User.finOne({email});
 if (userAvailable) {
 res.status(400);
-throw New Error("User already registered")
+throw new Error("User already registered") 
 }
 
- res.json({message: "Register user"})
+
+res.json({message: "Register user"})
 });
 
 // @desc Login a user
