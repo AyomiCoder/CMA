fba941e7e6f993cddf97f5a4dcdd787a63ba1d7b
AyomiCoder <alukostephen01@gmail.com>
2024-01-20 16:52:16 +0100

    password hash fix

--- controller/userController.js
+++ controller/userController.js
@@ -2,7 +2,6 @@ const asyncHandler = require('express-async-handler');
 const bcrypt = require('bcrypt')
 const User = require('../models/userModel')
 
-
 // @desc Register a user
 // @route POST api/users/register
 // @access public
@@ -12,14 +11,27 @@ if (!username || !email || !password){
 res.status(400);
 throw new Error("All fields are mandatory")
 }
-const userAvailable = await User.finOne({email});
+const userAvailable = await User.findOne({email});
 if (userAvailable) {
 res.status(400);
 throw new Error("User already registered") 
 }
+
 //Hashed Password
 const hashedPassword = await bcrypt.hash(password, 10);
-console.log(hashedPassword);
+console.log("Hashed Password:",hashedPassword);
+const user = await User.create({
+    username,
+    email,
+    password: hashedPassword,
+})
+console.log(`User created ${user}`)
+if (user){
+    res.status(200).json({_id: user.id, email: user.email})
+} else{
+    res.status(400)
+    throw new Error("User data not valid")
+}
 res.json({message: "Register user"})
 });
 
